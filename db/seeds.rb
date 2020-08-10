58e540ed105653ffae033ff5ad8e5c46d13e6e53
slee713 <steven.lee1393@gmail.com>
2020-08-10 00:56:43 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,6 @@ lisa = Patient.create({name: "Lisa Simpson", age: 8})
 maggie = Patient.create({name: "Maggie Simpson", age: 1})
 
 Appointment.create([
-  {appointment_datetime: DateTime.new(2016, 03, 15, 18, 00, 0), patient: homer, doctor: hawkeye},
   {appointment_datetime: DateTime.new(2016, 01, 11, 20, 20, 0), patient: bart, doctor: meredith},
   {appointment_datetime: DateTime.new(2016, 04, 22, 14, 00, 0), patient: marge, doctor: michaela},
   {appointment_datetime: DateTime.new(2017, 10, 30, 15, 00, 0), patient: lisa, doctor: phillip},
